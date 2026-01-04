4b8985a8606f5d85d0cad752e81d267fb272de17
thu <thu601925@gmail.com>
2026-01-04 13:15:31 +0700

    update code

---
+++ src/features/gantt/components/bars/index.ts
@@ -0,0 +1,5 @@
+// Task bar components
+export { DependencyLine } from './DependencyLine';
+export { MilestoneDiamond } from './MilestoneDiamond';
+export { ProgressBar } from './ProgressBar';
+export { TaskBar } from './TaskBar';
---
+++ src/features/gantt/components/timeline/index.ts
@@ -0,0 +1,5 @@
+// Timeline components
+export { ChartArea } from './ChartArea';
+export { TimelineGrid } from './TimelineGrid';
+export { TimelineHeader } from './TimelineHeader';
+export { TimeMarker } from './TimeMarker';
